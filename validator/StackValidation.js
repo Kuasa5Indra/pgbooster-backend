7186fed0bff39aa72d9ddc386711c87d0873a93f
Kuasa5Indra <adityaindra135@gmail.com>
2021-11-13 09:09:27 +0700

    few update stack validation

--- validator/StackValidation.js
+++ validator/StackValidation.js
@@ -3,10 +3,11 @@ const { body, query } = require("express-validator");
 exports.stackFormValidator = [
     body('name').notEmpty().withMessage('Stack Name is required'),
     body('codeFile').custom((value, { req }) => {
-        allowedMimetypes = ['application/json', 'text/yaml'];
+        filename = req.files.codeFile.name;
+        allowedFileExtension = ['.json', '.yaml', '.yml'];
         if(!req.files){
             throw new Error("File resource is required");
-        } else if (!allowedMimetypes.includes(req.files.codeFile.mimetype)){
+        } else if (!allowedFileExtension.some(ext => filename.includes(ext))){
             throw new Error("File resource must be json or yaml");
         }
 
