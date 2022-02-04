e745f719f77236d7893bef87f029694798be7082
indra <adityaindra135@gmail.com>
2022-02-04 23:41:12 +0700

    Add Template Validation Feature

--- validator/StackValidation.js
+++ validator/StackValidation.js
@@ -18,6 +18,20 @@ exports.stackFormValidator = [
     body('protect').optional({ nullable: true }).isBoolean().withMessage('Set protection is either true or false')
 ];
 
+exports.stackTemplateValidator = [
+    body('codeFile').custom((value, { req }) => {
+        filename = req.files.codeFile.name;
+        allowedFileExtension = ['.json', '.yaml', '.yml'];
+        if (!req.files) {
+            throw new Error("File resource is required");
+        } else if (!allowedFileExtension.some(ext => filename.includes(ext))) {
+            throw new Error("File resource must be json or yaml");
+        }
+
+        return true;
+    }),
+];
+
 exports.stackQueryValidator = [
     param('name').notEmpty().withMessage('Stack Name is required')
     .matches(/[a-zA-Z\d]-+/).withMessage('The stack name can include letters (A-Z and a-z), numbers (0-9), and hyphens (-).'),
